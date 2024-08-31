a4c1271a49b8af6786c59b1fd91fb37d0eefba4e
RTAkland <me@rtast.cn>
2024-08-31 22:29:05 +0800

    feat: add role var

--- src/main/kotlin/cn/rtast/rob/entity/Sender.kt
+++ src/main/kotlin/cn/rtast/rob/entity/Sender.kt
@@ -7,6 +7,7 @@
 
 package cn.rtast.rob.entity
 
+import cn.rtast.rob.enums.UserRole
 import com.google.gson.annotations.SerializedName
 
 data class Sender(
@@ -14,4 +15,5 @@ data class Sender(
     val userId: Long,
     val nickname: String,
     val sex: String,
+    val role: UserRole
 )
---
+++ src/main/kotlin/cn/rtast/rob/enums/UserRole.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2024 RTAkland
+ * Author: RTAkland
+ * Date: 2024/8/31
+ */
+
+
+package cn.rtast.rob.enums
+
+enum class UserRole {
+    admin, member
+}
