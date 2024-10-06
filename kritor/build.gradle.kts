04ae730c72bd7173ba2dac61ba53b1f910cf0c38
RTAkland <me@rtast.cn>
2024-10-06 13:38:05 +0800

    feat: add kritor dep

--- kritor/build.gradle.kts
+++ kritor/build.gradle.kts
@@ -1,2 +1,33 @@
+plugins {
+    id("com.google.protobuf") version "0.9.4"
+}
+
 dependencies {
+    implementation("io.grpc:grpc-protobuf:1.68.0")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+    implementation("io.grpc:grpc-netty-shaded:1.68.0")
+    implementation("com.google.protobuf:protobuf-kotlin:4.29.0-RC1")
+    protobuf(files("kritor/protos"))
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.21.12"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.68.0"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+        }
+    }
 }
