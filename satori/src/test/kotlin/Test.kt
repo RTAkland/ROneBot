fd818f0b6d325806159b39f251404bc49bd39aac
RTAkland <me@rtast.cn>
2024-10-09 17:24:20 +0800

    move comment

--- satori/src/test/kotlin/Test.kt
+++ satori/src/test/kotlin/Test.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2024 RTAkland
+ * Author: RTAkland
+ * Date: 2024/10/8
+ */
+
 import cn.rtast.rob.satori.RSatoriFactory
 import cn.rtast.rob.satori.entity.GroupMessage
 import cn.rtast.rob.satori.entity.GroupRevokeMessage
@@ -5,12 +11,6 @@ import cn.rtast.rob.satori.entity.LoginInfo
 import cn.rtast.rob.satori.entity.PrivateMessage
 import cn.rtast.rob.satori.entity.PrivateRevokeMessage
 import cn.rtast.rob.satori.util.SatoriListener
-
-/*
- * Copyright © 2024 RTAkland
- * Author: RTAkland
- * Date: 2024/10/8
- */
 
 class RSatori : SatoriListener {
     override suspend fun onReady(loginInfo: LoginInfo) {
