7147ddd7e30d033769ad863bf53157e8d4c777ae
RTAkland <me@rtast.cn>
2024-10-27 13:33:47 +0800

    feat: add kritor

---
+++ kritor/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "cn.rtast"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
