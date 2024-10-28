1b0d306689562d61d997220f7480d822c8421dbe
RTAkland <me@rtast.cn>
2024-10-28 20:08:59 +0800

    fix: fix ws server error

---
+++ onebot/src/test/kotlin/test/TestServer.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2024 RTAkland
+ * Author: RTAkland
+ * Date: 2024/10/28
+ */
+
+
+package test
+
+import cn.rtast.rob.ROneBotFactory
+
+fun main() {
+    val bot = ROneBotFactory.createServer(8888, "114514ghpA@", TestClient())
+}
