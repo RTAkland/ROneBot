e1eda51117661a68088caa00b2360e7249f2876c
RTAkland <me@rtast.cn>
2024-11-09 18:58:38 +0800

    chore: improve regex performance

--- ronebot-onebot-v11/src/main/kotlin/cn/rtast/rob/util/CommandManagerImpl.kt
+++ ronebot-onebot-v11/src/main/kotlin/cn/rtast/rob/util/CommandManagerImpl.kt
@@ -21,17 +21,16 @@ class CommandManagerImpl internal constructor() : CommandManager {
     override val permissionCommands = mutableListOf<PermissionCommand>()
     private val interceptor
         get() = if (!ROneBotFactory.isInterceptorInitialized) defaultInterceptor else ROneBotFactory.interceptor
-    private val commandList get() = commands.flatMap { it.commandNames }
-    private val permissionCommandList get() = permissionCommands.flatMap { it.commandNames }
-    private val commandRegex get() = Regex(commandList.joinToString("|") { "/?$it" })
-    private val permissionRegex get() = Regex(permissionCommandList.joinToString("|") { "/?$it" })
+    private val commandList
+        get() = commands.flatMap { it.commandNames }.toMutableList()
+            .apply { addAll(permissionCommands.flatMap { it.commandNames }) }
+    val commandRegex get() = Regex(commandList.joinToString("|") { "/?$it" })
 
     private fun getCommand(message: BaseMessage): Pair<Pair<BaseCommand?, String?>, Pair<PermissionCommand?, String?>> {
-        val commonCommandResult = commandRegex.find(message.text)?.value
-        val permissionCommandResult = permissionRegex.find(message.text)?.value
-        val matchedCommand = commands.find { it.commandNames.contains(commonCommandResult) }
-        val permissionMatchCommand = permissionCommands.find { it.commandNames.contains(permissionCommandResult) }
-        return (matchedCommand to commonCommandResult) to (permissionMatchCommand to permissionCommandResult)
+        val matchedCommand = commandRegex.find(message.text)?.value
+        val command = commands.find { it.commandNames.contains(matchedCommand) }
+        val permissionMatchCommand = permissionCommands.find { it.commandNames.contains(matchedCommand) }
+        return (command to matchedCommand) to (permissionMatchCommand to matchedCommand)
     }
 
     override suspend fun register(command: BaseCommand) {
