010fd44c37853f0367c8fce392f1a2adae9d1e79
RTAkland <me@rtast.cn>
2024-11-12 11:21:43 +0800

    chore: remove useless dep

--- ronebot-qqbot-legacy/build.gradle.kts
+++ ronebot-qqbot-legacy/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-    api(project(":ronebot-common"))
     api(project(":ronebot-common-ext"))
     api(libs.java.websocket)
 }
--- ronebot-qqbot-official/build.gradle.kts
+++ ronebot-qqbot-official/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-    api(project(":ronebot-common"))
     api(project(":ronebot-common-ext"))
     api(libs.ktor.server.core)
     api(libs.ktor.server.netty)
