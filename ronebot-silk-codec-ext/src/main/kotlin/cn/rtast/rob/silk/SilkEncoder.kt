91f5fa6803aa698f11b198ebfb8b5c8d073ffe95
RTAkland <me@rtast.cn>
2024-11-17 10:58:43 +0800

    chore: split encoder and decoder

--- ronebot-silk-codec-ext/src/main/kotlin/cn/rtast/rob/silk/SilkEncoder.kt
+++ ronebot-silk-codec-ext/src/main/kotlin/cn/rtast/rob/silk/SilkEncoder.kt
@@ -30,39 +30,3 @@ fun File.encodeToSilk(sampleRate: Int): ByteArray {
     SilkCoder.encode(this.inputStream(), outputStream, sampleRate)
     return outputStream.toByteArray()
 }
-
-/**
- * 从一个[InputStream]转换成PCM格式的音频, 但是需要参数传入
- */
-fun InputStream.decodeToSilk(sampleRate: Int, strict: Boolean, loss: Int): ByteArray {
-    val outputStream = ByteArrayOutputStream()
-    SilkCoder.decode(this, outputStream, strict, sampleRate, loss)
-    return outputStream.toByteArray()
-}
-
-/**
- * 从一个[File]转换成PCM格式的音频, 但是需要参数传入
- */
-fun File.decodeToSilk(sampleRate: Int, strict: Boolean, loss: Int): ByteArray {
-    val outputStream = ByteArrayOutputStream()
-    SilkCoder.decode(this.inputStream(), outputStream, strict, sampleRate, loss)
-    return outputStream.toByteArray()
-}
-
-/**
- * 从一个[InputStream]转换成PCM格式的音频
- */
-fun InputStream.decodeToSilk(): ByteArray {
-    val outputStream = ByteArrayOutputStream()
-    SilkCoder.decode(this, outputStream)
-    return outputStream.toByteArray()
-}
-
-/**
- * 从一个[File]对象转换成PCM格式的音频
- */
-fun File.decodeToSilk(): ByteArray {
-    val outputStream = ByteArrayOutputStream()
-    SilkCoder.decode(this.inputStream(), outputStream)
-    return outputStream.toByteArray()
-}
