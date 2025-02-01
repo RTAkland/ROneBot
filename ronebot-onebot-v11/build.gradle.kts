75e32fec7e67b7d222132ef47ee7f2b55cb864bb
RTAkland <me@rtast.cn>
2025-02-01 16:13:22 +0800

    chore[onebot11]: impl perm module

--- ronebot-onebot-v11/build.gradle.kts
+++ ronebot-onebot-v11/build.gradle.kts
@@ -1,10 +1,6 @@
-repositories {
-    maven("https://repo.maven.rtast.cn/releases/")
-}
-
 dependencies {
     api(libs.java.websocket)
     api(project(":ronebot-common"))
 
-    testImplementation("cn.rtast:ronebot-permission:2.6.10")
+    testImplementation(project(":ronebot-permission"))
 }
