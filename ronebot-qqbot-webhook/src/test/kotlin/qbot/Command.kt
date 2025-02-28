9320c901b19ed5219313db62a5b281369f8a48ac
RTAkland <rtakland@outlook.com>
2025-03-01 04:21:07 +0800

    chore[qqbot:test]: fix not impl

--- ronebot-qqbot-webhook/src/test/kotlin/qbot/Command.kt
+++ ronebot-qqbot-webhook/src/test/kotlin/qbot/Command.kt
@@ -6,7 +6,9 @@
 
 package qbot
 
+import cn.rtast.rob.entity.IMessageChain
 import cn.rtast.rob.qqbot.command.BaseCommand
+import cn.rtast.rob.qqbot.entity.inbound.C2CMessageCreateEvent
 import cn.rtast.rob.qqbot.entity.inbound.GroupAtMessageCreateEvent
 
 class TestCommand: BaseCommand() {
@@ -15,5 +17,37 @@ class TestCommand: BaseCommand() {
     override suspend fun executeGroup(message: GroupAtMessageCreateEvent, args: List<String>) {
         println(message.d.content)
         println("111")
+    }
+
+    override suspend fun C2CMessageCreateEvent.reject(reason: IMessageChain) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun GroupAtMessageCreateEvent.reject(reason: IMessageChain) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun onGroupSession(msg: GroupAtMessageCreateEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun onPrivateSession(msg: C2CMessageCreateEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun C2CMessageCreateEvent.skipSession() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun GroupAtMessageCreateEvent.skipSession() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun GroupAtMessageCreateEvent.startSession() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun C2CMessageCreateEvent.startSession() {
+        TODO("Not yet implemented")
     }
 }
