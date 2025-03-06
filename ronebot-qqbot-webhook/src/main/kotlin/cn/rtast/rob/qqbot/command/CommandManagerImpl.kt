6f7e43231815275851adaed8b28dfcf162625203
RTAkland <rtakland@outlook.com>
2025-03-06 17:40:18 +0800

    fix[qqbot]: update with common

--- ronebot-qqbot-webhook/src/main/kotlin/cn/rtast/rob/qqbot/command/CommandManagerImpl.kt
+++ ronebot-qqbot-webhook/src/main/kotlin/cn/rtast/rob/qqbot/command/CommandManagerImpl.kt
@@ -8,28 +8,22 @@ package cn.rtast.rob.qqbot.command
 
 import cn.rtast.rob.command.CommandManager
 import cn.rtast.rob.enums.MatchingStrategy
-import cn.rtast.rob.interceptor.Interceptor
 import cn.rtast.rob.qqbot.QBotFactory
 import cn.rtast.rob.qqbot.entity.inbound.C2CMessageCreateEvent
 import cn.rtast.rob.qqbot.entity.inbound.GroupAtMessageCreateEvent
 import cn.rtast.rob.qqbot.entity.inbound.command
-import cn.rtast.rob.qqbot.interceptor.defaultInterceptor
 import kotlin.reflect.KFunction
 
 public class CommandManagerImpl : CommandManager<BaseCommand, GroupAtMessageCreateEvent, C2CMessageCreateEvent> {
     override val commands: MutableList<BaseCommand> = mutableListOf<BaseCommand>()
     override var commandRegex: Regex = Regex("")
     override val functionCommands: MutableList<KFunction<*>> = mutableListOf<KFunction<*>>()
-    private val interceptor
-        get() =
-            if (!QBotFactory.isInterceptorInitialized) defaultInterceptor else QBotFactory.interceptor
-    private val _interceptor = Interceptor<BaseCommand, GroupAtMessageCreateEvent, C2CMessageCreateEvent>()
 
     override suspend fun handlePrivate(message: C2CMessageCreateEvent) {
         val matchedCommand = commandRegex.find(message.d.content)?.value
         val command = commands.find { command -> command.commandNames.any { it == message.command } }
         command?.let {
-            _interceptor.handlePrivateInterceptor(message, interceptor, it) {
+            QBotFactory.interceptor.handlePrivateInterceptor(message, it) {
                 command.handlePrivate(it, matchedCommand ?: "", MatchingStrategy.SPACES)
             }
         }
@@ -39,7 +33,7 @@ public class CommandManagerImpl : CommandManager<BaseCommand, GroupAtMessageCrea
         val matchedCommand = commandRegex.find(message.d.content)?.value
         val command = commands.find { command -> command.commandNames.any { it == message.command } }
         command?.let {
-            _interceptor.handleGroupInterceptor(message, interceptor, it) {
+            QBotFactory.interceptor.handleGroupInterceptor(message, it) {
                 command.handleGroup(it, matchedCommand ?: "", MatchingStrategy.SPACES)
             }
         }
