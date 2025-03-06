77d12a2d434c02fee239e53be34a9f0ec4c25383
RTAkland <rtakland@outlook.com>
2025-03-07 04:08:49 +0800

    fix[kook]: dokka document generate

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,13 +84,13 @@ subprojects {
         }
 
         repositories {
-//            maven {
-//                url = uri("https://maven.rtast.cn/releases/")
-//                credentials {
-//                    username = "RTAkland"
-//                    password = System.getenv("PUBLISH_TOKEN")
-//                }
-//            }
+            maven {
+                url = uri("https://maven.rtast.cn/releases/")
+                credentials {
+                    username = "RTAkland"
+                    password = System.getenv("PUBLISH_TOKEN")
+                }
+            }
 
             maven {
                 url = uri("https://maven.rtast.cn/snapshots/")
