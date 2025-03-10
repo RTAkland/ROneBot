d17df5e78d284c8d107039620e09a8ad53b17151
RTAkland <rtakland@outlook.com>
2025-03-11 07:20:12 +0800

    fix[starter-frontend]: fix frontend

--- ronebot-starter/starter-backend/src/main/kotlin/cn/rtast/rob/starter/backend/util/Resources.kt
+++ ronebot-starter/starter-backend/src/main/kotlin/cn/rtast/rob/starter/backend/util/Resources.kt
@@ -8,7 +8,6 @@ package cn.rtast.rob.starter.backend.util
 
 public object Resources {
     public fun load(path: String): ByteArray {
-        println(path)
         return this::class.java.classLoader.getResourceAsStream("templates/$path")!!.use { it.readBytes() }
     }
 }
