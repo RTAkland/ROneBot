60157210525ca1516bfbbeb9ea56a0c4226c035a
RTAkland <rtakland@outlook.com>
2025-03-13 05:11:45 +0800

    feat[starter-backend]: add more options

--- ronebot-starter/starter-backend/src/main/kotlin/cn/rtast/rob/starter/backend/Main.kt
+++ ronebot-starter/starter-backend/src/main/kotlin/cn/rtast/rob/starter/backend/Main.kt
@@ -58,6 +58,7 @@ public fun main() {
                     .replace("{{GROUP_ID}}", groupId)
                     .replace("{{KOTLIN_VERSION}}", kotlinVersion)
                     .replace("{{ROB_VERSION}}", robVersion)
+                    .replace("{{MAIN_CLASS}}", "$packageName.MakeKt")
                 val mainKt = String(Resources.load("Main.kt"))
                     .replace("{{APP_PACKAGE}}", packageName)
                 val settingsGradleKts = String(Resources.load("settings.gradle.kts"))
