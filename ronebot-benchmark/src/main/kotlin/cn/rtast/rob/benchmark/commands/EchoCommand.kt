fcbb101923c7f1898c2af31eabdcf19e36b51cd6
RTAkland <rtakland@outlook.com>
2025-03-15 05:59:42 +0800

    chore[benchmark]: update benchmark test

--- ronebot-benchmark/src/main/kotlin/cn/rtast/rob/benchmark/commands/EchoCommand.kt
+++ ronebot-benchmark/src/main/kotlin/cn/rtast/rob/benchmark/commands/EchoCommand.kt
@@ -9,11 +9,20 @@ package cn.rtast.rob.benchmark.commands
 import cn.rtast.rob.command.BaseCommand
 import cn.rtast.rob.event.raw.GroupMessage
 import cn.rtast.rob.event.raw.text
+import kotlinx.coroutines.delay
 
 public class EchoCommand : BaseCommand() {
     override val commandNames: List<String> = listOf("/echo")
 
     override suspend fun executeGroup(message: GroupMessage, args: List<String>) {
         println("echo ${message.text}")
+    }
+}
+
+public class TestDelayCommand : BaseCommand() {
+    override val commandNames: List<String> = listOf("/delay")
+
+    override suspend fun executeGroup(message: GroupMessage, args: List<String>) {
+        delay(1000L)
     }
 }
