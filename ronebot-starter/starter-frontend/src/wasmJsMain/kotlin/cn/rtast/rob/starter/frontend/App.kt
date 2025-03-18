3ec995259f53a4c7945e1a6a92432434fd92ce75
RTAkland <rtakland@outlook.com>
2025-03-18 20:47:54 +0800

    chore[starter-frontend]: format code

--- ronebot-starter/starter-frontend/src/wasmJsMain/kotlin/cn/rtast/rob/starter/frontend/App.kt
+++ ronebot-starter/starter-frontend/src/wasmJsMain/kotlin/cn/rtast/rob/starter/frontend/App.kt
@@ -18,8 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import cn.rtast.rob.starter.frontend.api.fetchLatestVersion
 import cn.rtast.rob.starter.frontend.api.submitFormData
