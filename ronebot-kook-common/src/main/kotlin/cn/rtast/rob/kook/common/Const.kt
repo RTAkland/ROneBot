d9d7e78b791402f3b7205e1f0f1540972f44d8e3
RTAkland <rtakland@outlook.com>
2025-03-22 07:39:54 +0800

    chore: remove kook modules

--- ronebot-kook-common/src/main/kotlin/cn/rtast/rob/kook/common/Const.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © 2025 RTAkland
- * Author: RTAkland
- * Date: 2025/3/6
- */
-
-package cn.rtast.rob.kook.common
-
-public const val KOOK_BASE_URL: String = "https://www.kookapp.cn/api/v3"
