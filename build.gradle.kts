5408c71a2b606cd229eb10a83c82a5f0a0a66f97
RTAkland <rtakland@outlook.com>
2025-03-29 16:11:21 +0800

    chore: now publishing to maven central!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 @file:OptIn(ExperimentalBCVApi::class)
 
 import cn.rtast.rob.buildSrc.excludeModuleNames
+import com.vanniktech.maven.publish.SonatypeHost
 import kotlinx.validation.ExperimentalBCVApi
 import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
 
 plugins {
-    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.signing)
+    alias(libs.plugins.vanniktech.maven.publish)
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.composeMultiplatform) apply false
@@ -31,50 +33,12 @@ allprojects {
 subprojects {
     if (name in excludeModuleNames) return@subprojects
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
-    apply(plugin = "maven-publish")
+    apply(plugin = "com.vanniktech.maven.publish")
+    apply(plugin = "signing")
     if (!project.name.contains("starter-backend")) {
         apply(plugin = "org.jetbrains.kotlin.multiplatform")
     } else {
         apply(plugin = "org.jetbrains.kotlin.jvm")
-    }
-
-    publishing {
-        repositories {
-            maven {
-                url = uri("https://maven.rtast.cn/releases/")
-                credentials {
-                    username = "RTAkland"
-                    password = System.getenv("PUBLISH_TOKEN")
-                }
-            }
-
-            publications.withType<MavenPublication> {
-                pom {
-                    description = "A Kotlin multiplatform library for OneBot11 development"
-                    url = "https://github.com/RTAkland/ROneBot"
-                    developers {
-                        developer {
-                            id = "rtakland"
-                            name = "RTAkland"
-                            email = "me@rtast.cn"
-                        }
-                    }
-
-                    licenses {
-                        license {
-                            name = "The Apache License, Version 2.0"
-                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                        }
-                    }
-
-                    scm {
-                        url = "https://github.com/RTAkland/ROneBot"
-                        connection = "scm:git:git://github.com/RTAkland/ROneBot.git"
-                        developerConnection = "scm:git:ssh://git@github.com/RTAkland/ROneBot.git"
-                    }
-                }
-            }
-        }
     }
 
     val wsAddress: String? by extra
@@ -92,6 +56,36 @@ subprojects {
         environment("WS_PASSWORD", wsPassword ?: "")
         environment("QQ_GROUP_ID", qqGroupId ?: "")
     }
+
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+        coordinates("cn.rtast.rob", project.name, libVersion)
+        pom {
+            description = "A Kotlin multiplatform library for OneBot11 development"
+            url = "https://github.com/RTAkland/ROneBot"
+            developers {
+                developer {
+                    id = "rtakland"
+                    name = "RTAkland"
+                    email = "me@rtast.cn"
+                }
+            }
+
+            licenses {
+                license {
+                    name = "The Apache License, Version 2.0"
+                    url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                }
+            }
+
+            scm {
+                url = "https://github.com/RTAkland/ROneBot"
+                connection = "scm:git:git://github.com/RTAkland/ROneBot.git"
+                developerConnection = "scm:git:ssh://git@github.com/RTAkland/ROneBot.git"
+            }
+        }
+    }
 }
 
 apiValidation {
@@ -99,4 +93,9 @@ apiValidation {
         enabled = true
     }
     nonPublicMarkers.add("cn.rtast.rob.annotations.InternalROBApi")
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
