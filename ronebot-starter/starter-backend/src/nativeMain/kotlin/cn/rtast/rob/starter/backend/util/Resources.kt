4e6dc1bae11c2d3b4f12fc3ca20f72ce041fa8f1
RTAkland <rtakland@outlook.com>
2025-04-01 12:49:12 +0800

    Nothing but update rkmbed to the latest version

--- ronebot-starter/starter-backend/src/nativeMain/kotlin/cn/rtast/rob/starter/backend/util/Resources.kt
+++ ronebot-starter/starter-backend/src/nativeMain/kotlin/cn/rtast/rob/starter/backend/util/Resources.kt
@@ -6,16 +6,10 @@
 
 package cn.rtast.rob.starter.backend.util
 
-import cn.rtast.rkmbed.runtime.RKMBedResource
-import cn.rtast.rkmbed.runtime.getResource
 import cn.rtast.rob.starter.backend.entity.Config
-import cn.rtast.rob.starter.backend.resources.GeneratedResource
+import cn.rtast.rob.starter.backend.resources.getResource
 
 object Resources {
-
-    init {
-        RKMBedResource.setResource(GeneratedResource)
-    }
 
     fun load(path: String): ByteArray {
         return getResource("templates/$path").asByteArray()
