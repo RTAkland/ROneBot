33ab18ca8ec99d403b95cb0029c0b0a45c438d7d
RTAkland <rtakland@outlook.com>
2025-04-05 23:33:25 +0800

    Use companion object to compat for java user(maybe....)

--- ronebot-onebot-v11/src/commonMain/kotlin/cn/rtast/rob/OneBotFactory.kt
+++ ronebot-onebot-v11/src/commonMain/kotlin/cn/rtast/rob/OneBotFactory.kt
@@ -22,140 +22,139 @@ import kotlin.time.Duration.Companion.seconds
 
 
 /**
- * OneBot协议的入口点所有Bot实例都从
- * 此静态类中创建
+ * 创建客户端的入口点
  */
-public object OneBotFactory : BotFactory {
-    /**
-     * 所有命令执行次数
-     */
-    override var totalCommandExecutionTimes: Int = 0
+public class OneBotFactory {
+    public companion object : BotFactory {
+        /**
+         * 所有命令执行次数
+         */
+        override var totalCommandExecutionTimes: Int = 0
 
-    /**
-     * 私聊命令执行次数
-     */
-    override var privateCommandExecutionTimes: Int = 0
+        /**
+         * 私聊命令执行次数
+         */
+        override var privateCommandExecutionTimes: Int = 0
 
-    /**
-     * 群聊命令执行次数
-     */
-    override var groupCommandExecutionTimes: Int = 0
+        /**
+         * 群聊命令执行次数
+         */
+        override var groupCommandExecutionTimes: Int = 0
 
-    /**
-     * 所有Bot实例的管理器
-     */
-    public val botManager: BotManager = BotManager()
+        /**
+         * 所有Bot实例的管理器
+         */
+        public val botManager: BotManager = BotManager()
 
-    /**
-     * 动态的获取所有的Bot实例
-     */
-    @Deprecated("该属性已废弃, 请使用botManager.allBots()", replaceWith = ReplaceWith("botManager.allBots()"))
-    internal val botInstances get() = botManager.botInstances.keys.toList()
+        /**
+         * 动态的获取所有的Bot实例
+         */
+        @Deprecated("该属性已废弃, 请使用botManager.allBots()", replaceWith = ReplaceWith("botManager.allBots()"))
+        internal val botInstances get() = botManager.botInstances.keys.toList()
 
-    /**
-     * 全局作用域的任务调度器但是这个调度器执行任务
-     * 并不会判断Bot实例是否已经初始化完成
-     * 所以你需要自行[forEach]然后判断每个Bot实例
-     * 是否已经初始化完成, 否则会抛出错误
-     */
-    public val globalScheduler: GlobalCoroutineScheduler<BotInstance> = GlobalCoroutineScheduler(botManager.allBots())
+        /**
+         * 全局作用域的任务调度器但是这个调度器执行任务
+         * 并不会判断Bot实例是否已经初始化完成
+         * 所以你需要自行[forEach]然后判断每个Bot实例
+         * 是否已经初始化完成, 否则会抛出错误
+         */
+        public val globalScheduler: GlobalCoroutineScheduler<BotInstance> =
+            GlobalCoroutineScheduler(botManager.allBots())
 
-    /**
-     * 在全局作用域的命令管理器
-     */
-    public val commandManager: CommandManagerImpl = CommandManagerImpl()
+        /**
+         * 在全局作用域的命令管理器
+         */
+        public val commandManager: CommandManagerImpl = CommandManagerImpl()
 
-    /**
-     * 通过继承来实现命令的方式的会话管理器
-     */
-    public val sessionManager: SessionManager = SessionManager()
+        /**
+         * 通过继承来实现命令的方式的会话管理器
+         */
+        public val sessionManager: SessionManager = SessionManager()
 
-    /**
-     * 获取所有的Bot实例数量
-     */
-    @Deprecated("该属性已废弃, 请使用botManager.allBots().size", replaceWith = ReplaceWith("botManager.allBots().size"))
-    public val botInstanceCount: Int get() = botManager.botInstances.size
+        /**
+         * 获取所有的Bot实例数量
+         */
+        @Deprecated(
+            "该属性已废弃, 请使用botManager.allBots().size",
+            replaceWith = ReplaceWith("botManager.allBots().size")
+        )
+        public val botInstanceCount: Int get() = botManager.botInstances.size
 
-    /**
-     * 全局作用域的指令拦截器
-     */
-    internal var interceptor: CommandInterceptor = defaultInterceptor
+        /**
+         * 全局作用域的指令拦截器
+         */
+        internal var interceptor: CommandInterceptor = defaultInterceptor
 
-    /**
-     * 设置BaseCommand的拦截器
-     */
-    public fun setInterceptor(interceptor: CommandInterceptor) {
-        this.interceptor = interceptor
-    }
+        /**
+         * 设置BaseCommand的拦截器
+         */
+        public fun setInterceptor(interceptor: CommandInterceptor) {
+            this.interceptor = interceptor
+        }
 
-    /**
-     * 将BaseCommand的拦截器设置为初始值
-     */
-    public fun clearInterceptor() {
-        this.interceptor = defaultInterceptor
-    }
+        /**
+         * 将BaseCommand的拦截器设置为初始值
+         */
+        public fun clearInterceptor() {
+            this.interceptor = defaultInterceptor
+        }
 
-    /**
-     * 获取BaseCommand的拦截器
-     */
-    public fun getInterceptor(): CommandInterceptor = interceptor
+        /**
+         * 获取BaseCommand的拦截器
+         */
+        public fun getInterceptor(): CommandInterceptor = interceptor
 
-    /**
-     * 创建一个Websocket客户端连接到OneBot实现
-     * 返回一个创建的Bot实例对象
-     */
-    public suspend fun createClient(
-        address: String,
-        accessToken: String,
-        listener: OneBotListener = object : OneBotListener {},
-        reconnectInterval: Duration = 3.seconds,
-        autoReconnect: Boolean = true,
-        messageExecuteDuration: Duration = 0.seconds,
-        logLevel: LogLevel = LogLevel.INFO
-    ): BotInstance {
-        val instance =
-            BotInstance(
-                address, accessToken, listener,
-                autoReconnect, 0, InstanceType.Client,
-                "/", reconnectInterval, messageExecuteDuration,
+        /**
+         * 创建一个Websocket客户端连接到OneBot实现
+         * 返回一个创建的Bot实例对象
+         */
+        public suspend fun createClient(
+            address: String,
+            accessToken: String,
+            listener: OneBotListener = object : OneBotListener {},
+            reconnectInterval: Duration = 3.seconds,
+            autoReconnect: Boolean = true,
+            messageExecuteDuration: Duration = 0.seconds,
+            logLevel: LogLevel = LogLevel.INFO
+        ): BotInstance {
+            val instance =
+                BotInstance(
+                    address, accessToken, listener,
+                    autoReconnect, 0, InstanceType.Client,
+                    "/", reconnectInterval, messageExecuteDuration,
+                    logLevel
+                ).createBot()
+            botManager.addBotInstance(instance)
+            return instance
+        }
+
+        /**
+         * 监听一个指定的端口来监听Websocket连接
+         * 让OneBot实现作为客户端连接到ROB
+         * 返回一个创建的Bot实例对象
+         */
+        public suspend fun createServer(
+            port: Int,
+            accessToken: String,
+            listener: OneBotListener = object : OneBotListener {},
+            path: String = "/",
+            messageExecuteDuration: Duration = 0.seconds,
+            logLevel: LogLevel = LogLevel.INFO
+        ): BotInstance {
+            // 这里的 127.0.0.1并没有任何作用, 仅仅是为了当作占位符使用
+            // 实际上 Websocket 服务器监听的是 `::` 包括了ipv4 和 ipv6
+            val instance = BotInstance(
+                "127.0.0.1", accessToken, listener,
+                true, port, InstanceType.Server,
+                path, 0.seconds, messageExecuteDuration,
                 logLevel
             ).createBot()
-        botManager.addBotInstance(instance)
-        return instance
-    }
-
-    /**
-     * 监听一个指定的端口来监听Websocket连接
-     * 让OneBot实现作为客户端连接到ROB
-     * 返回一个创建的Bot实例对象
-     */
-    public suspend fun createServer(
-        port: Int,
-        accessToken: String,
-        listener: OneBotListener = object : OneBotListener {},
-        path: String = "/",
-        messageExecuteDuration: Duration = 0.seconds,
-        logLevel: LogLevel = LogLevel.INFO
-    ): BotInstance {
-        // 这里的 127.0.0.1并没有任何作用, 仅仅是为了当作占位符使用
-        // 实际上 Websocket 服务器监听的是 `::` 包括了ipv4 和 ipv6
-        val instance = BotInstance(
-            "127.0.0.1", accessToken, listener,
-            true, port, InstanceType.Server,
-            path, 0.seconds, messageExecuteDuration,
-            logLevel
-        ).createBot()
-        botManager.addBotInstance(instance)
-        return instance
+            botManager.addBotInstance(instance)
+            return instance
+        }
     }
 
     override fun toString(): String {
         return "OneBotFactory"
     }
 }
-
-/**
- * 向下兼容SDK而添加的类型别名
- */
-@Deprecated("该名称已废弃, 请使用OneBotFactory", replaceWith = ReplaceWith("cn.rtast.rob.OneBotFactory"))
-public typealias ROneBotFactory = OneBotFactory
--- ronebot-qqbot-webhook/src/commonMain/kotlin/cn/rtast/rob/qqbot/QBotFactory.kt
+++ ronebot-qqbot-webhook/src/commonMain/kotlin/cn/rtast/rob/qqbot/QBotFactory.kt
@@ -17,42 +17,53 @@ import cn.rtast.rob.qqbot.entity.inbound.GroupAtMessageCreateEvent
 import cn.rtast.rob.qqbot.qbot.QQBotListener
 import cn.rtast.rob.scheduler.GlobalCoroutineScheduler
 
-public object QBotFactory : BotFactory {
+public class QBotFactory {
+    public companion object : BotFactory {
+        /**
+         * 命令管理器
+         */
+        public val commandManager: CommandManagerImpl = CommandManagerImpl()
 
-    public val commandManager: CommandManagerImpl = CommandManagerImpl()
+        /**
+         * 所有Bot实例
+         */
+        internal val botInstances = mutableListOf<BotInstance>()
 
-    internal val botInstances = mutableListOf<BotInstance>()
+        /**
+         * 全局作用域的指令拦截器, 只能有一个拦截器
+         */
+        public lateinit var interceptor: ICommandInterceptor<BaseCommand, GroupAtMessageCreateEvent, C2CMessageCreateEvent>
 
-    /**
-     * 全局作用域的指令拦截器, 只能有一个拦截器
-     */
-    public lateinit var interceptor: ICommandInterceptor<BaseCommand, GroupAtMessageCreateEvent, C2CMessageCreateEvent>
+        /**
+         * 判断拦截器是否已经初始化
+         */
+        internal val isInterceptorInitialized get() = ::interceptor.isInitialized
 
-    /**
-     * 判断拦截器是否已经初始化
-     */
-    internal val isInterceptorInitialized get() = ::interceptor.isInitialized
+        /**
+         * 全局作用域的任务调度器
+         */
+        public val globalScheduler: GlobalCoroutineScheduler<BotInstance> = GlobalCoroutineScheduler(botInstances)
 
-    /**
-     * 全局作用域的任务调度器
-     */
-    public val globalScheduler: GlobalCoroutineScheduler<BotInstance> = GlobalCoroutineScheduler(botInstances)
+        /**
+         * 创建HTTP服务器 / 只能在QQBot webhook中使用这种方式
+         */
+        public suspend fun createServer(
+            port: Int,
+            appId: String,
+            clientSecret: String,
+            listener: QQBotListener
+        ): BotInstance {
+            val instance = BotInstance(port, appId, clientSecret, listener).apply { createBot() }
+            botInstances.add(instance)
+            return instance
+        }
 
-    /**
-     * 创建HTTP服务器 / 只能在QQBot webhook中使用这种方式
-     */
-    public suspend fun createServer(
-        port: Int,
-        appId: String,
-        clientSecret: String,
-        listener: QQBotListener
-    ): BotInstance {
-        val instance = BotInstance(port, appId, clientSecret, listener).apply { createBot() }
-        botInstances.add(instance)
-        return instance
+        override var totalCommandExecutionTimes: Int = 0
+        override var privateCommandExecutionTimes: Int = 0
+        override var groupCommandExecutionTimes: Int = 0
     }
 
-    override var totalCommandExecutionTimes: Int = 0
-    override var privateCommandExecutionTimes: Int = 0
-    override var groupCommandExecutionTimes: Int = 0
+    override fun toString(): String {
+        return "QBotFactory"
+    }
 }
