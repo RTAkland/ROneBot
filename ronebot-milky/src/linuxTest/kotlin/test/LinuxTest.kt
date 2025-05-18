ef0a55bb7f9d701cdeb4e9290e4bb0e3d4a918d2
RTAkland <me@rtast.cn>
2025-05-18 20:22:46 +0800

    Add linux test

---
+++ ronebot-milky/src/linuxTest/kotlin/test/LinuxTest.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2025 RTAkland & 小满1221
+ * Date: 5/18/25, 8:22 PM
+ * Open Source Under Apache-2.0 License
+ * https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package test
+
+import cn.rtast.rob.milky.MilkyBotFactory
+import cn.rtast.rob.milky.enums.internal.APIEndpoint
+import cn.rtast.rob.milky.event.system.GetLoginInfo
+import cn.rtast.rob.milky.util.requestAPI
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+
+class LinuxTest {
+    @Test
+    fun `test milky on linux`() {
+        runBlocking {
+            val bot = MilkyBotFactory.createBot("http://127.0.0.1:8080", "114514")
+            println(bot.requestAPI<GetLoginInfo>(APIEndpoint.System.GetLoginInfo))
+        }
+    }
+}
