d0a25952e1657ec00b426739fe7d536e6cf28753
RTAkland <me@rtast.cn>
2025-05-21 07:52:04 +0800

    Add placeholder event

---
+++ ronebot-milky/src/commonMain/kotlin/cn/rtast/rob/milky/event/ws/raw/DummyEvent.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2025 RTAkland & 小满1221
+ * Date: 5/21/25, 7:51 AM
+ * Open Source Under Apache-2.0 License
+ * https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package cn.rtast.rob.milky.event.ws.raw
+
