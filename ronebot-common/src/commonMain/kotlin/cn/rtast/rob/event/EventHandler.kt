20d5178cd511b8d4836cfd2a06462f1237d1152d
kuku <kuku@kuku.me>
2025-06-27 06:33:41 +0000

    support registers multiple events of the same type

--- ronebot-common/src/commonMain/kotlin/cn/rtast/rob/event/EventHandler.kt
+++ ronebot-common/src/commonMain/kotlin/cn/rtast/rob/event/EventHandler.kt
@@ -15,7 +15,7 @@ import kotlin.reflect.KClass
 /**
  * 一个map存储了所有注册的消息处理器
  */
-public val eventHandlers: MutableMap<BaseBotInstance, MutableMap<KClass<out BaseDispatchEvent<*>>, suspend (BaseDispatchEvent<*>) -> Unit>> =
+public val eventHandlers: MutableMap<BaseBotInstance, MutableMap<KClass<out BaseDispatchEvent<*>>, MutableList<suspend (BaseDispatchEvent<*>) -> Unit>>> =
     mutableMapOf()
 
 /**
@@ -25,9 +25,14 @@ public inline fun <reified T : BaseDispatchEvent<*>> BaseBotInstance.onEvent(cro
     val eventType = T::class
     val botEventHandlers = eventHandlers[this]
     if (botEventHandlers != null) {
-        botEventHandlers[eventType] = { event -> handler(event as T) }
+        var events = botEventHandlers[eventType]
+        if (events == null) {
+            events = mutableListOf()
+            botEventHandlers[eventType] = events
+        }
+        events.add({ event -> handler(event as T) })
     } else {
-        eventHandlers[this] = mutableMapOf(eventType to { event -> handler(event as T) })
+        eventHandlers[this] = mutableMapOf(eventType to mutableListOf({ event -> handler(event as T) }))
     }
 }
 
@@ -39,7 +44,7 @@ public suspend fun BaseBotInstance.dispatchEvent(event: BaseDispatchEvent<out Se
     val botEventHandlers = eventHandlers[this]
     if (botEventHandlers != null) {
         val handler = botEventHandlers[event::class]
-        handler?.invoke(event)
+        handler?.forEach { it.invoke(event) }
     }
 }
 
